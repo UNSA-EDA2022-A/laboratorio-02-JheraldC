f7c0bf79c94e71893341765fe856cd59dd96b6e0
JHERALD HUREN CACERES APAZA <93649530+JheraldC@users.noreply.github.com>
2022-05-15 02:09:07 -0500

    Update Exercise1.java

--- src/main/java/com/example/project/Exercise1.java
+++ src/main/java/com/example/project/Exercise1.java
@@ -1,6 +1,5 @@
 package com.example.project;
-
-import java.util.Scanner;
+import java.util.*;
 
 public class Exercise1 {
 
@@ -10,12 +9,22 @@ public class Exercise1 {
 		Scanner sc = new Scanner(System.in);
 		while (true) {
 			int n = sc.nextInt();
-			System.out.println(obj.esCuadradoPerfecto(n));			
+			System.out.println(obj.esCuadradoPerfecto(n));
 		}
 	}
 
 	public boolean esCuadradoPerfecto(int numero) {
-
-		return false;
+		return checkCuadradoPerfecto(1, numero);
+	}
+	
+	public boolean checkCuadradoPerfecto(int aux, int num) {
+		while(aux*aux <= num) {
+			if(((num % 1) == 0) && ((num / (double)aux) == (double)aux)){
+				return true;
+			} else {
+				return checkCuadradoPerfecto(aux + 1, num);
+			}
+		}
+		return false;	
 	}
 }
